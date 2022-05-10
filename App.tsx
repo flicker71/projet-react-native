e70c4e1179165743ec10699504f184432fa3d1a4
flicker71 <72727849+flicker71@users.noreply.github.com>
2022-05-10 09:28:30 +0200

    route api

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Footer from './componants/Footer/Footer';
 import Header from './componants/Header/Header';
 import { globalStyles } from './Styles/style';
+import axios from "axios";
+
+
 
 
 export type AppNavigator = {
@@ -14,9 +17,22 @@ export type AppNavigator = {
 }  
 
 function HomeScreen({ navigation }:any) {
+  
+  const [dessert, setDessert] = useState([]);
+
+  
+  useEffect(() => {
+    (async () => {
+      const data = await axios.get("https://random-data-api.com/api/dessert/random_dessert?size=10")
+      setDessert(data.data)
+    })()
+  }, []);
+console.log(dessert);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      
+      
+      <Text>Home Screen {dessert && dessert[0] && dessert[0].flavor}</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
@@ -39,32 +55,44 @@ function DetailsScreen({ navigation }:any) {
   );
 }
 
+
 const Stack = createNativeStackNavigator<AppNavigator>();
 
 export default function App() {
-
+  
+  
   const [headerName, setHeaderName] = useState<string>('Header');
 
   const changerHeaderName = ((title:string) => {
     setHeaderName( title );
   })
 
-  return (
-    <View style={globalStyles.container}>
-      <Header title={headerName}/>
-      <Text>Metro - boulot - dodo</Text>
-      <Button title='Accueil' onPress={() => changerHeaderName('Accueil')}/>
-      <Button title='Autres' onPress={() => changerHeaderName('Autres')}/>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-        
-      <Footer/>
-    </View>
+  
+    return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    );
+  }
 
-  );
-}
+//   return (
+//     <View style={globalStyles.container}>
+//       {/* <Header title={headerName}/>
+//       <Text>Metro - boulot - dodo</Text>
+//       <Button title='Accueil' onPress={() => changerHeaderName('Accueil')}/>
+//       <Button title='Autres' onPress={() => changerHeaderName('Autres')}/>
+//       <StatusBar style="auto" />
+//       <Footer/> */}
+    
+//       <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//       </Stack.Navigator>
+//       </NavigationContainer>
+//     </View>
+
+//   );
+// }
